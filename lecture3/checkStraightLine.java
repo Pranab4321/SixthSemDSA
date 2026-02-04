220935638e10f12a2f7312c7cbd6df978d171a36
Pranab <mahanandapranab@gmail.com>
2026-02-04 09:55:39 +0530

    adding two java program

---
+++ lecture3/checkStraightLine.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class checkStraightLine{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        int p = 22/7;
+        System.out.println(p);
+
+        int m1 = (y2-y1)/(x2-x1);
+        int m2 = (y3-y2)/(x3-x2);
+
+        if(m1==m2){
+            System.out.println("This is a straignt line");
+        }else{
+            System.out.println("This is not a straignt line");
+        }
+    }
+}
---
+++ lecture3/loteryGame.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class loteryGame{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n>=300 && n<=460){
+            System.out.println("The prize is MacBook.");
+        }else if(n>=200 && n<=280){
+            System.out.println("The prize is Kurkure.");
+        }else if(n>=1100 && n<=1500){
+            System.out.println("The prize is Cycle.");
+        }else if(n>=50 && n<=80){
+            System.out.println("The prize is Bike.");
+        }else{
+            System.out.println("Better luck next time.");
+        }
+    }
+}
